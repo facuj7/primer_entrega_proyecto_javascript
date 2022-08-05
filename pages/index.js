60eb3a1c39a1ebaf025a950864b2c3040954db1e
Facundo Juárez <facujuarez663@gmail.com>
2022-08-04 21:43:43 -0300

    cambios con libreria en nombres de tipologias

--- pages/index.js
+++ pages/index.js
@@ -35,18 +35,22 @@
         {id: 1, tipologia: "THOUSE", precio: 50000, habitaciones: 2, baños: 1, descuento: 10000, foto: "fotocasa1"},
         {id: 2, tipologia: "TALLHOUSE", precio: 70000, habitaciones: 3, baños: 2, descuento: 15000, foto: "fotocasa2"},
         {id: 3, tipologia: "EVOHOUSE", precio: 100000, habitaciones: 4, baños: 3, descuento: 20000, foto: "foto casa3"},
-    ]
+    ];
     
+    /* EJEMPLO PARA BUSCAR INFORMACIÓN DE LA TIPOLOGÍA DESEADA*/
+    let productoEncontrado = productos.find(el => el.id === 3);
+    console.log(productoEncontrado);
+
     const modelo = [];
     let producto
-    let entrada = prompt("Ingrese el nombre de su producto para ver detalles o escriba ESC para salir:\nTHOUSE\nTALLHOUSE\nEVOHOUSE").toUpperCase()
+    let entrada = prompt("Ingrese el nombre de su producto para ver detalles o escriba ESC para salir:\nTHOUSE\nTALLHOUSE\nEVOHOUSE").toUpperCase();
     
     while (entrada !="ESC"){
-        producto = productos.find(item => item.tipologia === entrada);
+        producto = productos.find(item => item.id === entrada);
         modelo.push(producto);
-        entrada = prompt("Ingrese el nombre de su producto para ver detalles o escriba ESC para salir:\nTHOUSE\nTALLHOUSE\nEVOHOUSE").toUpperCase()
+        entrada = prompt("Ingrese el nombre de su producto para ver detalles o escriba ESC para salir:\nTHOUSE\nTALLHOUSE\nEVOHOUSE").toUpperCase();
     }
-
+    
     /*prueba con if*/
     
     alert(`A continuación, más detalles de la tipología seleccionada`)
@@ -55,8 +59,8 @@
     console.log(modelo)
 
     /*Evento Input*/
-/*let saludo = document.getElementById("saludo");
-let nombre = document.getElementById("nombre");
+    /*let saludo = document.getElementById("saludo");
+    let nombre = document.getElementById("nombre");
     
     nombre.addEventListener("input", () => {
         if(nombre.value.includes("tallhouse")){
@@ -113,15 +117,23 @@ for(const producto of productos){
 
 let descu = document.getElementById("descuento");    
 /* CREACION DIV DESCUENTO */
-let calcu = document.createElement("h2");
-
-calcu.innerHTML = "<h2>Calculadora para descuento</h2>";
-
-descu.append(calcu);
 
 let nombre = document.getElementById("nombre");
 nombre.addEventListener("input", (e) =>{
-})
+}) 
+
+let tipo = "THOUSE";
+
+if(tipo === "THOUSE"){
+    console.log("tipologia: THOUSE, precio: 50000, habitaciones: 2, baños: 1, descuento: 10000, foto: fotocasa1");
+}else if(tipo === "TALLHOUSE"){
+    console.log("tipologia: TALLHOUSE, precio: 70000, habitaciones: 3, baños: 2, descuento: 15000, foto: fotocasa2");
+}else if(tipo === "EVOHOUSE"){
+    console.log("tipologia: EVOHOUSE, precio: 100000, habitaciones: 4, baños: 3, descuento: 20000, foto: fotocasa3");
+}else{
+    console.log("ingresa una tipología existente")
+} 
+
 
 /* Probando Storage - JSON */
 
@@ -134,5 +146,40 @@ localStorage.setItem("objeto", JSON.stringify(objeto)); */
 let objeto = localStorage.getItem("objeto");
 console.log(JSON.parse(objeto));
 
+/*Aplicando libreria*/
 
+let button = document.getElementById("boton1");
 
+button.addEventListener("click", () => { 
+    Swal.fire({
+      icon: 'ok',
+      title: 'TIPOLOGIA: TALLHOUSE',
+      text: "Precio: $70.000, Habitaciones: 3, Baños: 2, Descuento: $15.000", 
+      timer: 10000,
+      imageUrl: 'https://placeholder.pics/svg/500x400',
+      
+    });
+
+let button = document.getElementById("boton2");
+button.addEventListener("click", () => { 
+    Swal.fire({
+        icon: 'ok',
+        title: 'TIPOLOGIA: THOUSE',
+        text: "Precio: $50.000, Habitaciones: 2, Baños: 1, Descuento: $10.000", 
+        timer: 10000,
+        imageUrl: 'https://placeholder.pics/svg/500x400',
+          
+        });
+        let button = document.getElementById("boton3");
+button.addEventListener("click", () => { 
+    Swal.fire({
+        icon: 'ok',
+        title: 'TIPOLOGIA: EVOHOUSE',
+        text: "Precio: $100.000, Habitaciones: 4, Baños: 3, Descuento: $20.000", 
+        timer: 10000,
+        imageUrl: 'https://placeholder.pics/svg/500x400',
+          
+            });
+        });
+    });
+});
